4adc1643aa15c0e43b41d9cd1390cd0fb0c21a89
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-05-15 23:18:44 +0100

    Add Project Files

---
+++ script.js
@@ -0,0 +1,11 @@
+//====== Pseudoclasses Recap! =======//
+
+
+
+
+
+//====== Pseudoclass Challenge! =======//
+// 1- Create a Vehicle pseudoclass that has a method of info().
+// 2- Create a Car pseudoclass that inherets Vehicle. Add a method of drive(). Add some relevant properties.
+// 3- Create a Tesla pseudoclass that inherets Car. Add some relevant properties.
+// 4- Create a Truck pseudoclass that inherets Vehicle. Add some relevant properties.
