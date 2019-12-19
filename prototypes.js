0475c1d5490ce77b9fe6b09e4f77e84c7fbd54a8
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-12-19 10:17:42 +0000

    .

--- prototypes.js
+++ prototypes.js
@@ -12,11 +12,11 @@ function Animal(attributes) {
 
 Animal.prototype.eat = function() {
   return `${this.name}: I eat ${this.food}`;
-}
+};
 
 Animal.prototype.speak = function() {
   return `${this.name}: ${this.sound}`;
-}
+};
 
 function Bird(attributes) {
   Animal.call(this, attributes);
@@ -32,7 +32,7 @@ Bird.prototype.fly = function() {
   } else {
     return `${this.name}: I'm attending flying lessons!`;
   }
-}
+};
 
 const eagle = new Bird({
   name: "Golden Eagle",
@@ -43,7 +43,7 @@ const eagle = new Bird({
   food: "Rabbit",
   sound: "Eaaaak",
   wingspan: "2 Meters",
-  canFly: true,
+  canFly: true
 });
 
 const penguin = new Bird({
@@ -67,11 +67,58 @@ console.log(penguin.fly());
 console.log(penguin.speak());
 console.log(penguin.eat());
 
-
-
 //====== Prototypes Challenge! =======//
 // 1- Create a Vehicle pseudoclass that has a method of info().
 // 2- Create a Car pseudoclass that inherets Vehicle. Add a method of drive(). Add some relevant properties.
 // 3- Create a Truck pseudoclass that inherets Vehicle. Add some relevant properties.
-// 4- Create a tesla instance of the Car pseudoclass. Add some relevant properties.
-// 5- Create a volvo instance of the Truck pseudoclass. Add some relevant properties.
+// 4- Initialise a tesla instance of the Car pseudoclass. Add some relevant property values.
+// 5- Initialise a volvo instance of the Truck pseudoclass. Add some relevant property values.
+
+// Hasan
+function Vehicle(attributes) {
+  this.color = attributes.color;
+  this.design = attributes.design;
+  this.engineSize = attributes.engineSize;
+}
+
+Vehicle.prototype.info = function() {
+  return `Vehicle: ${this.color}, ${this.design}, ${this.engineSize}`;
+};
+
+Vehicle.prototype.drive = function() {};
+
+function Car(attributes) {
+  Vehicle.call(this, attributes);
+  this.seatsCount = attributes.seatsCount;
+}
+
+Car.prototype = Object.create(Vehicle.prototype);
+
+function Truck(attributes) {
+  Vehicle.call(this, attributes);
+  this.loadCapacity = attributes.loadCapacity;
+}
+
+Truck.prototype = Object.create(Vehicle.prototype);
+
+function Tesla(attributes) {
+  Car.call(this, attributes);
+  this.chargingVoltage = attributes.chargingVoltage;
+}
+
+Tesla.prototype = Object.create(Car.prototype);
+
+function Volvo(attributes) {
+  Truck.call(this, attributes);
+}
+
+Volvo.prototype = Object.create(Truck.prototype);
+
+const volvo = new Volvo({
+  color: "darkblue",
+  design: "suv",
+  seatsCount: 3,
+  engineSize: '1.8 L'
+});
+
+console.log(volvo);
