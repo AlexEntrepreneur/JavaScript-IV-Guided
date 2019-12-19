5368a20aacda6ca75503996546077f4ace928aee
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-12-19 09:34:25 +0000

    .

--- prototypes.js
+++ prototypes.js
@@ -1,6 +1,50 @@
 //====== Prototypes Recap! =======//
+// Animal => Bird => Eagle/Penguin
+function Animal(attributes) {
+  this.name = attributes.name;
+  this.size = attributes.size;
+  this.weight = attributes.weight;
+  this.habitat = attributes.habitat;
+  this.color = attributes.color;
+  this.food = attributes.food;
+  this.sound = attributes.sound;
+}
 
+Animal.prototype.eat = function() {
+  return `${this.name}: I eat ${this.food}`;
+}
 
+Animal.prototype.speak = function() {
+  return `${this.name}: ${this.sound}`;
+}
+
+function Bird(attributes) {
+  Animal.call(this, attributes);
+  this.wingspan = attributes.wingspan;
+  this.canFly = attributes.canFly;
+}
+
+Bird.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.fly = function() {
+  if (this.canFly) {
+    return `${this.name}: I believe I can fly!`;
+  } else {
+    return `${this.name}: I'm attending flying lessons!`;
+  }
+}
+
+const eagle = new Bird({
+  name: "Golden Eagle",
+  size: "Really Big",
+  weight: "Lightweight",
+  habitat: "The Mountains",
+  color: "Brownish",
+  food: "Rabbit",
+  sound: "Eaaaak",
+  wingspan: "2 Meters",
+  canFly: true,
+})
 
 
 
